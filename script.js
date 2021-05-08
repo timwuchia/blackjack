e61b464095132cf13ab04ff4e7659c7f8acdf93b
unknown <timwuchia@gmail.com>
2021-05-08 10:14:34 -0700

    make card A value able to adjusted to 1

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ deck.shuffle();
 const playerHand = new PlayerHand;
 const dealerHand = new DealerHand;
 let dealerFaceDownCardValue = 0;
+let dealerASetToOne = false;
+let playerASetToOne = false;
 let playerValue = 0;
 let dealerValue = 0;
 
 const dealerDeck = document.querySelector('.dealer-side');
 const playerDeck = document.querySelector('.player-side');
+const dealerCards = document.querySelector('.dealer-side .dealer-cards');
+const playerCards = document.querySelector('.player-side .player-cards');
 const dealerFaceValue = document.querySelector('.dealer-side #dealer-value');
 const playerFaceValue = document.querySelector('.player-side #player-value');
 const hitBtn = document.querySelector('.hit-btn');
 const standBtn = document.querySelector('.stand-btn');
+const newRoundBtn = document.querySelector('.new-round-btn');
 const message = document.querySelector('.deck .message');
+
+document.addEventListener("DOMContentLoaded", () => {
+    startRound();
+})
 
 const setDealerFaceValue = (value) => {
     dealerFaceValue.innerHTML = value;
@@ -24,6 +33,9 @@ const setDealerFaceValue = (value) => {
 const setPlayerFaceValue = (value) => {
     playerFaceValue.innerHTML = value;
 }
+
+
+
 
 const playerLost = () => {
     message.innerHTML = 'Sorry, you lost';
@@ -37,10 +49,10 @@ const tieGame = () => {
     message.innerHTML = "It's a tie!!";
 }
 
-const cardFaceValue = (cardValue, setAToOne) => {
+const cardFaceValue = (cardValue, aIsOne) => {
     switch(cardValue){
         case "A":
-            return !setAToOne ? 11 : 1;
+            return !aIsOne ? 11 : 1;
             break;
         case "2":
             return 2;
@@ -81,17 +93,49 @@ const flipDealerCard = () => {
 }
 
 const addCard = (side, card) => {
+    console.log(deck)
     if(side === 'player'){
         playerHand.hand.push(card)
-        playerDeck.append(card.getHTML());
-        console.log(playerHand)
+        playerCards.append(card.getHTML());
     }
     if(side === 'dealer'){
         dealerHand.hand.push(card)
-        dealerDeck.append(card.getHTML());
-        
+        dealerCards.append(card.getHTML());
     } 
 }
+
+const enableButton = (btn) => {
+    switch(btn){
+        case "hit" :
+            hitBtn.disabled = false;
+            break;
+        case "stand":
+            standBtn.disabled = false;
+            break;
+        case "new-round":
+            newRoundBtn.disabled = false;
+            break;
+    }
+}
+
+const clearHands = () => {
+    dealerCards.innerHTML = "";
+    playerCards.innerHTML = "";
+    message.innerHTML = "";
+    playerHand.hand = [];
+    dealerHand.hand = [];
+}
+
+newRoundBtn.addEventListener('click', function(){
+    setDealerFaceValue(0);
+    setPlayerFaceValue(0);
+    playerValue = 0;
+    dealerValue = 0;
+    playerASetToOne = false;
+    dealerASetToOne = false;
+    clearHands();
+    startRound();
+})
 
 const startRound = () => {
     const initDealerCard = deck.popTwoCards();
@@ -104,13 +148,6 @@ const startRound = () => {
         playerValue = playerValue + cardFaceValue(item.value, false);
         setPlayerFaceValue(playerValue)
     })
-    // playerHand.initCard(initPlayerCard);
-    // dealerHand.initCard(initDealerCard);
-    // playerHand.hand.map((item) => {
-    //     addCard('player', item);
-    //     playerValue = playerValue + cardFaceValue(item.value);
-    //     setPlayerFaceValue(playerValue)
-    // })
     addCard('dealer', initDealerCard[0]);
     addCard('dealer', initDealerCard[1]);
     dealerValue = dealerValue + cardFaceValue(dealerHand.hand[0].value, false);
@@ -124,15 +161,21 @@ hitBtn.addEventListener('click', () => {
     playerValue = playerValue + cardFaceValue(addedCard.value, false);
     setPlayerFaceValue(playerValue)
     if(playerValue > 21){
+        if(handHasA(playerHand.hand) === true && playerASetToOne === false){
+            playerValue = playerValue - 10;
+            playerASetToOne = true;
+            setPlayerFaceValue(playerValue);
+            return;
+        }
         playerLost();
+        enableButton('new-round');
+        return;
     }
-    console.log(playerValue);
 })
 
 const handHasA = (hand) => {
-    // console.log(hand)
     for(let i = 0; i < hand.length; i++){
-        if(hand.value === "A") return true;
+        if(hand[i].value === "A") return true;
     }
     return false;
 }
@@ -142,46 +185,44 @@ setTimeout(() => {
 }, 1000)
 
 
-
 standBtn.addEventListener('click', async () => {
     flipDealerCard();
-    
-    const addCardToDealer = async (setAToOne) => {
-        return new Promise((resolve, reject) => {
-            const interval = setInterval(() => {
-                const addedCard = deck.popCard();
-                addCard('dealer', addedCard);
-                dealerValue = dealerValue + cardFaceValue(addedCard.value, setAToOne);
-                setDealerFaceValue(dealerValue)
-                console.log(dealerValue)
-                if(dealerValue > 17 && dealerValue > playerValue) clearInterval(interval);
-            }, 1500);
-            // return
-        })
+    const sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
     }
-    console.log('jasdfjashdfjk')
-    const res = await addCardToDealer(false);
+
+    while(dealerValue < 17 || dealerValue < playerValue){
+        const addedCard = deck.popCard();
+        addCard('dealer', addedCard);
+        dealerValue = dealerValue + cardFaceValue(addedCard.value, false);
+        setDealerFaceValue(dealerValue)
+        // addingCardToDealer()
+        await sleep(1500)
+    }
     
     if(dealerValue > 21){
-        if(handHasA(dealerHand.hand)){
-            dealerValue = dealerValue - 10
+        if(handHasA(dealerHand.hand) === true && dealerASetToOne === false){
+            dealerValue = dealerValue - 10;
+            dealerASetToOne = true;
             setDealerFaceValue(dealerValue);
             await addCardToDealer(true);
             if(dealerValue > 21) {
                 playerWon();
+                enableButton('new-round');
                 return;
             }
             if(dealerValue < playerValue) {
                 playerWon();
+                enableButton('new-round');
                 return;
             }
             
         } else {
             playerWon();
+            enableButton('new-round');
+            return
         }
     }
-    console.log('player lost')
     playerLost();
+    enableButton('new-round');
 })
-
-startRound();
